2a37a843f87b09030265d0fa80f9a7be4c2c0a65
Bobby Coleman <colemankidding@gmail.com>
2020-05-01 19:59:40 -0700

    replicated cat database and added for each loop for the cats in database to display on cats index page

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Cat:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
 
 def home (request):
     return render(request, 'home.html')
@@ -10,4 +22,4 @@ def about (request):
     return render(request, 'about.html')
 
 def cats_index (request):
-    return render(request, 'cats/index.html')
+    return render(request, 'cats/index.html', { 'cats' : cats })
