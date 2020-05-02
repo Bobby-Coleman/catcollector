b47127ce2714368bc1baa823a5da1190e2a85455
Bobby Coleman <colemankidding@gmail.com>
2020-05-01 19:52:46 -0700

    add cats index and /cats route

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 
 
 def home (request):
-    return HttpResponse('<h1>Hello there<h1>')
+    return render(request, 'home.html')
 
 def about (request):
     return render(request, 'about.html')
+
+def cats_index (request):
+    return render(request, 'cats/index.html')
