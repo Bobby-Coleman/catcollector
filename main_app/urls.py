7bf351d2ec2bd0ce2cb8661d567715bb8817afee
Bobby Coleman <colemankidding@gmail.com>
2020-05-04 11:58:03 -0700

    Use class based view for cats index, rename cats under templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/', views.cats_index, name='index'),
+    path('cats/', views.CatList.as_view(), name='cats_index'),
     path('cats/<int:cat_id>', views.cats_detail, name='cats_detail'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
 from .models import Cat
-
-
-# class Cat:
-#   def __init__(self, name, breed, description, age):
-#     self.name = name
-#     self.breed = breed
-#     self.description = description
-#     self.age = age
-
-# cats = [
-#   Cat('Lolo', 'tabby', 'foul little demon', 3),
-#   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-#   Cat('Raven', 'black tripod', '3 legged cat', 4)
-# ]
+from django.views.generic import ListView
 
 def home (request):
     return render(request, 'home.html')
@@ -21,9 +8,15 @@ def home (request):
 def about (request):
     return render(request, 'about.html')
 
-def cats_index (request):
-    cats = Cat.objects.all()
-    return render(request, 'cats/index.html', { 'cats' : cats })
+# def cats_index (request):
+#     cats = Cat.objects.all()
+#     return render(request, 'cats/index.html', { 'cats' : cats })
+class CatList(ListView):
+    model = Cat
+
+    def get_queryset(self):
+        return Cat.objects.all()
+
 
 def cats_detail (request, cat_id):
     cat = Cat.objects.get(id=cat_id)
