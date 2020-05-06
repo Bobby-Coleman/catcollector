c376e40cb2c3a15ec6165a679df414b70d583183
Bobby Coleman <colemankidding@gmail.com>
2020-05-06 12:01:04 -0700

    Create Cat class

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -10,4 +16,15 @@ class Cat(models.Model):
         return self.name
     def get_absolute_url(self):
         return reverse('cats_detail', kwargs={'pk':self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+        )
+    def __str__(self):
+        return f"{self.get_meal_display() on {self.date}}"
 
