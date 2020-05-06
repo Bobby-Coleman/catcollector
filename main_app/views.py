a2ab5c35554548bb54e72a174bc408ccd3b8a082
Bobby Coleman <colemankidding@gmail.com>
2020-05-06 16:59:42 -0700

    Added working feeding form to cat detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cat
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -23,17 +23,13 @@ def cat_detail(request, pk):
         'feeding_form': feeding_form
     })
     
-def add_feeding(request, cat_id):
-	# create the ModelForm using the data in request.POST
-  form = FeedingForm(request.POST)
-  # validate the form
-  if form.is_valid():
-    # don't save the form to the db until it
-    # has the cat_id assigned
-    new_feeding = form.save(commit=False)
-    new_feeding.cat_id = cat_id
-    new_feeding.save()
-  return redirect('detail', cat_id=cat_id)
+def add_feeding(request, pk):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = pk
+        new_feeding.save()
+    return redirect('cats_detail', pk=pk)
 
 
 class CatCreate(CreateView):
