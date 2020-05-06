586d838b96e5d813eb2838accf186dc3510b099e
Bobby Coleman <colemankidding@gmail.com>
2020-05-06 16:00:45 -0700

    caught up with class, changed cat_detail to show feedings

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path('',                      views.home,                name='home'),
     path('about/',                views.about,               name='about'),
     path('cats/',                 views.CatList.as_view(),   name='cats_index'),
-    path('cats/<int:pk>',         views.cats_detail, name='cats_detail'),
+    path('cats/<int:pk>',         views.cat_detail,          name='cats_detail'),
     path('cats/create/',          views.CatCreate.as_view(), name='cats_create'),
     path('cats/<int:pk>/update',  views.CatUpdate.as_view(), name='cats_update'),
     path('cats/<int:pk>/delete',  views.CatDelete.as_view(), name='cats_delete'),
+    # path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -15,13 +15,26 @@ class CatList(ListView):
     def get_queryset(self):
         return Cat.objects.all()
 
-def cats_detail(request, pk):
+def cat_detail(request, pk):
     cat = Cat.objects.get(id=pk)
     feeding_form = FeedingForm()
     return render(request, 'main_app/cat_detail.html', {
         'cat': cat,
         'feeding_form': feeding_form
     })
+    
+def add_feeding(request, cat_id):
+	# create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
 
 class CatCreate(CreateView):
     model = Cat
