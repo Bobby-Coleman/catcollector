67b8a5e5f980f798503db636b5d0ead1b9ed6e5a
Bobby Coleman <colemankidding@gmail.com>
2020-05-06 13:27:22 -0700

    Add Cat foreign key relationship to Feeding, make migrations for Feeding

--- main_app/models.py
+++ main_app/models.py
@@ -25,6 +25,8 @@ class Feeding(models.Model):
         choices=MEALS,
         default=MEALS[0][0]
         )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f"{self.get_meal_display() on {self.date}}"
+        return f"{self.get_meal_display()} on {self.date}"
 
